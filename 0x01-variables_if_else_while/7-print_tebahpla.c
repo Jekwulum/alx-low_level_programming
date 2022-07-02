0ec48540d3884658993e635f79042ad7fd09900c
Charles Nwoye <charlesnwoye2@gmail.com>
2022-07-01 17:31:40 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char val;
+
+	for (val = 'z'; val >= 'a'; val--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
