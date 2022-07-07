852d9fe81c309fe1eecd6e873ea8982d073635ad
Charles Nwoye <charlesnwoye2@gmail.com>
2022-07-07 03:59:40 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int c;
+
+	c = 0;
+
+	while (c < n)
+	{
+		if (c <= 0)
+			break;
+		
+		_putchar('_');
+
+		c++;
+	}
+	_putchar('\n');
+}
