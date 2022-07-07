44993b8c31baf717b0811f1bba2bc6f834b9efdf
Charles Nwoye <charlesnwoye2@gmail.com>
2022-07-07 04:19:10 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,11 +5,14 @@ i#include "main.h"
  */
 void print_line(int n)
 {
-int i;
 
-for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	int i;
+	
+	for (i = 0; i < n; i++)
 	{
-	_putchar('_');
+		_putchar('_');
 	}
 	_putchar('\n');
 }
