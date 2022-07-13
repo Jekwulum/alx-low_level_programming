064dda3a06c16f10864c46d266a468fa08ae9b09
Charles Nwoye <charlesnwoye2@gmail.com>
2022-07-12 18:55:42 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - outputs a string
+ * @str: char array type
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i])
+	}
+	_putchar('\n');
+}
