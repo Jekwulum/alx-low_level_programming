4f5ea7023641b8ce38ffd1fef464fa4a1504138d
Charles Nwoye <charlesnwoye2@gmail.com>
2022-07-12 19:53:24 -0700

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: int type
+ */
+
+int main(void)
+{
+	int randd, count, total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		randd = (rand() % 125) + 1;
+		printf("%c", randd);
+		total -= randd;
+	}
+	print("%c", total);
+
+	return (0);
+}
